62ca1e5e63734e1ce55fe37a65147c22e09ab318
Ruslan <ruslan.ogarkov88@gmail.com>
2020-06-15 21:46:48 +0300

    changing score result

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         // Check if the player won the game 
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('actve');
             document.getElementById('name-' + activePlayer).textContent = 'Winner';
             document.querySelector('.dice').style.display = 'none';
